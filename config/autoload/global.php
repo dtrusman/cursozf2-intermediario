99ee2a16dc304b440a21227d9653a28cec3bd1b1
Daniel Trusman <dtrusman@gmail.com>
2016-02-14 16:57:10 -0200

    Abstract Service e Mail

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,16 @@
  */
 
 return array(
-    // ...
+    'mail' => array(
+        'name' => 'smtp.googlemail.com',
+        'host' => 'smtp.googlemail.com',
+        'connection_class' => 'login',
+        'connection_config' => array(
+            'username' => 'dtrusman@gmail.com',
+            'password' => '123',
+            'ssl' => 'tls',
+            'port' => 465,
+            'from' => 'dtrusman@gmail.com'
+        )
+    )
 );
